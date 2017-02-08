75e31815cb89fec599c36794cd69b1d5efc6ca4d
iYung <ivankhyung@gmail.com>
2017-02-08 17:17:17 -0500

    added globals with step size in init

--- stepperControl.py
+++ stepperControl.py
@@ -9,12 +9,26 @@ import time as t
 
 class stepper(object):
   
+  #pattern required to turn the steppers
+  Seq = [[1,0,0,1],
+	 [1,0,0,0],
+         [1,1,0,0],
+         [0,1,0,0],
+         [0,1,1,0],
+         [0,0,1,0],
+         [0,0,1,1],
+         [0,0,0,1]] 
+  
+  #length of the sequence
+  StepCount = len(Seq)
+  
   #params
   ##boardMode --type on pin numbering, true means board, else BCM
   ##pin1-4 --pins for the stepper
   ##clockwise --sets movement to be clockwise, conterclock wise if false
-  def __init__(self, boardMode=None, pin1=None ,pin2=None, pin3=None, pin4=None, clockwise=None):
-    
+  ##precise --if true, sets stepper for 8 bit movement. else, 4 bit movement
+  def __init__(self, boardMode=None, pin1=None ,pin2=None, pin3=None, pin4=None, clockwise=None, precise=None):
+
     #set to correct mode
     if boardMode:
       print("Board mode")
@@ -36,6 +50,15 @@ class stepper(object):
       #GPIO.output(pin, False)
     
     #stores direction
-    self.clockwise = clockwise
+    if clockwise:
+      self.direction = 1
+    else:
+      self.direction = -1
+    
+    #sets step size with precise
+    if precise:
+      self.stepSize = 1
+    else:
+      self.stepSize = 2
     
 var = stepper(1,1,2,3,4,1)
