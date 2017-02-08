d23caa24e95284def09f3f378ea0c93caddca147
iYung <ivankhyung@gmail.com>
2017-02-08 16:55:12 -0500

    starting motor class init

--- stepperControl.py
+++ stepperControl.py
@@ -8,7 +8,23 @@ Documentation, License etc.
 import time as t
 
 class stepper(object):
-  def __init__(self):
-    print("Stepper created")
+  
+  #params
+  ##boardMode --type on pin numbering, true means board, else BCM
+  ##pin1.4 -- pins for the stepper
+  def __init__(self, boardMode=None, pin1=None ,pin2=None, pin3=None, pin4=None):
+    
+    #set to correct mode
+    if boardMode:
+      print("Board mode")
+      #GPIO.setmode(GPIO.BOARD)
+    else:
+      print("BCM mode")
+      #GPIO.setmode(GPIO.BCM)
+      
+    #remove warnings
+    #GPIO.setwarnings(False)
+    
+    self.pins = [pin1,pin2,pin3,pin4]
     
 var = stepper()
