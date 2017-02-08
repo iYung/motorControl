361d58e595ea32629adeb4e8487312b9aae1b99f
iYung <ivankhyung@gmail.com>
2017-02-08 15:05:23 -0500

    created class motor

---
+++ stepperControl.py
@@ -0,0 +1,14 @@
+'''
+Documentation, License etc.
+
+@package motorControl
+'''
+
+#import RPi.GPIO as GPIO
+import time as t
+
+class stepper(object):
+  def __init__(self):
+    print("Stepper created")
+    
+var = stepper()
